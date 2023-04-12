44393e7ae0ece26f7769c734817723af1d7728f7
LinneaGab <gali22mt@student.ju.se>
2023-04-12 05:38:06 +0200

    fixed localstorage

--- todo.js
+++ todo.js
@@ -26,22 +26,68 @@ class Task extends HTMLElement {
 
     this.querySelector("#delete-button").addEventListener("click", () => {
       this.remove();
+      saveTasks();
     });
   }
   //toggle tasks
   toggle() {
     this.done = !this.done;
     this.querySelector(".task-title").classList.toggle("done");
+    saveTasks();
   }
 }
 //custom element
 customElements.define("task-item", Task);
 
-//adding tasks
-const INPUT = document.querySelector("#inputbox");
-document.querySelector("#add-button").addEventListener("click", () => {
-  if (INPUT.value === "") return;
+//save tasks to local storage
+function saveTasks() {
+  let tasks = [];
+  document.querySelectorAll(".task").forEach((task) => {
+    tasks.push({
+      title: task.querySelector(".task-title").innerText,
+      done: task.querySelector(".task-title").classList.contains("done"),
+    });
+  });
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
 
-  new Task(INPUT.value);
-  INPUT.value = "";
-});
+//load tasks from local storage
+function loadTasks() {
+  //get tasks from local storage
+  const TASKS = JSON.parse(localStorage.getItem("tasks"));
+  if (TASKS) {
+    //create tasks from data
+    TASKS.forEach((task) => {
+      const TASK = new Task(task.title);
+      if (task.done) {
+        //apply done status
+        TASK.toggle();
+      }
+    });
+  }
+}
+
+//adding tasks
+function loadHandler() {
+  const INPUT = document.querySelector("#inputbox");
+  INPUT.focus();
+  loadTasks();
+  //add tasks with button
+  document.querySelector("#add-button").addEventListener("click", () => {
+    if (INPUT.value === "") return;
+    new Task(INPUT.value);
+    INPUT.value = "";
+    INPUT.focus();
+
+    saveTasks();
+  });
+  //add tasks with enter key, used ChatGPT
+  INPUT.addEventListener("keyup", (event) => {
+    if (event.keyCode === 13) {
+      event.preventDefault();
+      document.querySelector("#add-button").click();
+    }
+  });
+}
+
+window.addEventListener("load", loadHandler);
